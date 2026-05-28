cb97a023b2d8f570506d3cdcb4192bfbe66ecea0
Jaitej <cjaitej@gmail.com>
2026-05-28 23:27:33 +0530

    renamed home to index

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -34,7 +34,7 @@ export default function TabsLayout() {
       }}
     >
       <Tabs.Screen
-        name="home"
+        name="index"
         options={{
           title: "Home",
           tabBarIcon: ({ color, size }) => (
