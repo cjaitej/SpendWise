a815e2a005e7509de805469eeec74c83113724f4
Jaitej <cjaitej@gmail.com>
2026-06-01 14:42:52 +0530

    build setup

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,9 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativewind } = require("nativewind/metro");
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Add ONNX model support
+config.resolver.assetExts.push("onnx");
 
 module.exports = withNativewind(config);
